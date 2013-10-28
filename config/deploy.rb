6b59b2a43f6c14d6c9253e2d904388bca08c578e
aviflombaum <avi@flombaum.com>
2013-10-28 11:21:39 -0400

    avis deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,19 @@
+require 'bundler/capistrano' # for bundler support
+
 set :application, "sinatra-students"
-set :repository,  ""
+set :repository,  "git@github.com:flatiron-school/sinatra-students-ruby-003.git"
+set :branch, "deployment"
+set :user, 'deploy'
+set :deploy_to, "/home/#{ user }/#{ application }"
+set :use_sudo, false
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :scm, :git
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+default_run_options[:pty] = true
+
+role :web, "162.243.77.146"                          # Your HTTP server, Apache/etc
+role :app, "162.243.77.146"                          # This may be the same as your `Web` server
+role :db,  "162.243.77.146", :primary => true # This is where Rails migrations will run
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -16,10 +22,10 @@ role :db,  "your slave db-server here"
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+ task :start do ; end
+ task :stop do ; end
+ task :restart, :roles => :app, :except => { :no_release => true } do
+   run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+ end
+end
