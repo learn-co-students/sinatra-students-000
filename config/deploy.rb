93fb9b52fd0fce4f01978ea72176fef7424a5c68
aviflombaum <avi@flombaum.com>
2013-10-28 15:36:43 -0400

    capify

---
+++ config/deploy.rb
@@ -0,0 +1,36 @@
+require 'bundler/capistrano' # for bundler support
+
+set :application, "sinatra-students-ruby-003" # This should mirror what you put in your NGinx Conf
+
+set :user, 'deploy' # Whatever the User You Make on Your Server
+
+set :repository,  "git@github.com:flatiron-school/sinatra-students-ruby-003.git" # The address of your fork's clone URL
+
+set :branch, "deployment-lecture"
+
+set :deploy_to, "/home/#{ user }/#{ application }"
+set :use_sudo, false
+
+set :scm, :git
+
+default_run_options[:pty] = true
+
+set :server_ip, '162.243.77.86' # This should be your server IP
+role :web, "#{server_ip}"                          # Your HTTP server, Apache/etc
+role :app, "#{server_ip}"                          # This may be the same as your `Web` server
+role :db,  "#{server_ip}", :primary => true        # This is where Rails migrations will run
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+ task :start do ; end
+ task :stop do ; end
+ task :restart, :roles => :app, :except => { :no_release => true } do
+   run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+ end
+end
