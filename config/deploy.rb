1d225e6f8594c862f960b6871a5a1ac68433778e
aviflombaum <avi@flombaum.com>
2013-10-28 16:10:04 -0400

    deploy from this

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,20 +21,24 @@ role :app, "#{server_ip}"                          # This may be the same as you
 role :db,  "#{server_ip}", :primary => true        # This is where Rails migrations will run
 
 # if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+before "deploy:restart", "deploy:symlink_database"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
+ task :symlink_database, :roles => :app do
+  run "ln -nfs #{shared_path}/students-production.db #{current_path}/db/students-production.db"
+ end
+
  task :migrate, :roles => :app do 
   run "cd #{current_path} && rake db:migrate RACK_ENV=production"
  end
 
  task :scrape_students, :roles => :app do
   run "cd #{current_path} && rake scrape_students RACK_ENV=production"
-end
+ end
  task :start do ; end
  task :stop do ; end
  task :restart, :roles => :app, :except => { :no_release => true } do
